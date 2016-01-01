b1d8e02378426c3bdefafd3b16a1245156460425
Shane Logsdon <shane@shanelogsdon.com>
2016-01-01 08:54:47 -0500

    working model

--- lib/hello.ex
+++ lib/hello.ex
@@ -4,22 +4,25 @@ defmodule Hello do
   alias Hello.TemplateSource
 
   def new(opts \\ []) do
+    destination = opts[:destination] || "."
     source = opts[:source] || "gogs:shane/elixir-hello"
     source_list = [:GitHub, :Gogs, :HTTP]
-    name = opts[:name] || "new"
+    template = opts[:template] || "new"
+    name = opts[:name]
+    |> String.split("_")
+    |> Enum.map(&String.capitalize/1)
+    |> Enum.join
+    bindings = opts
+    |> Keyword.put(:app, opts[:name])
+    |> Keyword.put(:module, name)
 
     source
     |> parse_source(source_list)
-    |> Retriever.read(name)
+    |> Retriever.read(template)
     |> Retriever.parse
-    |> Template.parse
-    |> Template.execute
-    # |> or_alert
-  end
-
-  def or_alert({:ok, _}), do: true
-  def or_alert({:error, reason}) do
-    raise reason |> to_string
+    |> Template.parse(bindings)
+    |> Template.prepare_files
+    |> Template.execute(destination)
   end
 
   def parse_source(source, [ts | t]) do
@@ -27,8 +30,8 @@ defmodule Hello do
     prefix = (template_source |> Map.get(:prefix)) <> ":"
     if source |> String.starts_with?(prefix) do
       path = source
-             |> String.replace(prefix, "")
-             |> String.split("/")
+      |> String.replace(prefix, "")
+      |> String.split("/")
       {:ok, TemplateSource.parse_path(template_source, path)}
     else
       parse_source(source, t)
--- lib/hello/template.ex
+++ lib/hello/template.ex
@@ -1,11 +1,42 @@
 defmodule Hello.Template do
   require EEx
 
-  def execute({:error, _} = m), do: m
-  def execute({:ok, template}) do
-    {:ok, template}
+  @re ~r{\n?<!-- START FILE ([a-zA-Z0-9/_\-.]+) -->\n}
+
+  def execute({:error, _} = m, _), do: m
+  def execute({:ok, files}, destination) do
+    files
+    |> Enum.zip(Stream.iterate(destination, &(&1)))
+    |> Enum.map(&create_file/1)
+
+    {:ok, files}
+  end
+  defp create_file({{name, contents}, destination}) do
+    path = "./" <> destination <> "/" <> name
+    |> Path.expand
+
+    path
+    |> Path.dirname
+    |> File.mkdir_p
+
+    path
+    |> File.write(contents, [:write])
   end
 
-  def parse({:error, _} = m), do: m
-  def parse({:ok, eex}), do: {:ok, EEx.eval_string(eex, [])}
+  def parse({:error, _} = m, _), do: m
+  def parse({:ok, eex}, bindings) do
+    {:ok, EEx.eval_string(eex, assigns: bindings)}
+  end
+
+  def prepare_files({:error, _} = m), do: m
+  def prepare_files({:ok, template}) do
+    files = @re
+    |> Regex.scan(template, capture: :all_but_first)
+    |> List.flatten
+    |> Enum.map(&(String.strip(&1, ?/)))
+    contents = @re
+    |> Regex.split(template)
+    |> Enum.filter(fn s -> s != "" end)
+    {:ok, Enum.zip(files, contents)}
+  end
 end
